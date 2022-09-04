c10e081cf39aa7d387519db74c575e9fa7c293c6
Jack Davies <67157618+jd1618@users.noreply.github.com>
2022-09-04 10:04:25 +0100

    Update expected_turns.py

--- Probability_Problems/Revealing_every_card/expected_turns.py
+++ Probability_Problems/Revealing_every_card/expected_turns.py
@@ -2,21 +2,16 @@
 from random import shuffle
 
 def expected_turns(deck):
-    
+
     '''
-    A function that given a list of distinct positive integers 
-    returns the expected number of times you need to shuffle the
-    list and observe the first entry until you have seen every 
-    element. 
-    
-    count - keeps track of the number of elements you have seen,
-    turns - keeps track of the number of turns you play this game, 
-    cards_seen - records the elements you have seen in a set.
+    A function that given a list of distinct positive integers 'deck'
+    returns the expected number of times you need to rearrange the list 
+    and observe the first entry until you have seen every element.
     '''
-    
+
     count,turns=0,0
     cards_seen=set()
-    
+
     while count<len(deck):
         if deck[0] not in cards_seen:
             cards_seen.add(deck[0])
@@ -25,12 +20,28 @@ def expected_turns(deck):
         turns+=1
     return turns
 
-# Setting up the problem
-deck=[x for x in range(int(input("Enter the size of the deck of cards: ")))]
-average_turns,simulations=0,int(input("Enter the number of simulations: "))
+# Setting up the problem with exceptions
+while True:
+  try:
+    n=input("Enter the number of cards in the deck: ")
+    n=int(n)
+    break
+  except ValueError:
+    print("Not a valid input for the number of cards! "
+    +"Please try again ...")
+
+while True:
+  try:
+    sims=input("Enter the number of simulations: ")
+    sims=int(sims)
+    break
+  except ValueError:
+    print("Not a valid input for the number of simulations! "
+    +"Please try again ...")
+avg_turns,deck=0,[x for x in range(n)]
 
 # Performing Monte Carlo simulations
-for _ in range(simulations):
-    average_turns+=expected_turns(deck)
-average_turns/=simulations
-print(int(average_turns))
+for _ in range(sims):
+    avg_turns+=expected_turns(deck)
+avg_turns/=sims
+print(int(avg_turns))
