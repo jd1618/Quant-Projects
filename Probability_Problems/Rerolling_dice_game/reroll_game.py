eaf4183a49deff1eeaaf2e82b7311586bac21b0b
Jack Davies <67157618+jd1618@users.noreply.github.com>
2022-09-04 08:45:18 +0100

    Update reroll_game.py

--- Probability_Problems/Rerolling_dice_game/reroll_game.py
+++ Probability_Problems/Rerolling_dice_game/reroll_game.py
@@ -5,18 +5,19 @@ def reroll_game(dice,n):
 
   '''
   A function that given a list of positive integers as input,
-  and a positive integer n, returns the outcome of a dice game,
-  where you are given the option, but not required, to reroll
-  the dice n times
+  and a positive integer n, returns a random list entry,
+  where you are given the option, but not the requirement, 
+  to repeat this process up to n times 
 
   expect - expected value of the game,
-  lesser_frequency - the number of integers less than expect,
-  greater_sum - the sum of numbers greater than expect
+  p - the number of integers less than expect,
+  tmp - the sum of numbers greater than expect
   '''
 
+  # Expectation for a dice game without rerolling
   expect=sum(dice)/len(dice)
-  
-  # Define temporary variables p and tmp
+
+  # Define temporary variables p and tmp
   p,tmp=0,0
 
   # Loop to generate expectation depending on rerolls
@@ -27,21 +28,26 @@ def reroll_game(dice,n):
           elif num>=expect:
               tmp+=num
       expect=(p/len(dice))*expect+(tmp/len(dice))
-      p,tmp=0,0
-  print(expect)
 
+      # Reset the game to recalculate expectation
+      p,tmp=0,0
+
+  # Proceed to play the game
   for _ in range(n+1):
       shuffle(dice)
       if dice[0]>=expect:
           return dice[0]
   return dice[0]
 
+# Input number of re-rolls and simulations
 n=int(input("Enter the number of re-rolls: "))
 simulations=int(input("Enter the number of simulations: "))
-dice=[x for x in range(1,7)]
-total=0
+
+
 # Perform Monte Carlo simulations
+total,dice=0,[x for x in range(1,7)]
 for _ in range(simulations):
     total+=reroll_game(dice,n)
 
 print(total/simulations)
+
